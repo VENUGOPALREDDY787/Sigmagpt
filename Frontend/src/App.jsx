9a59964a2258a648e218690e20d12dbb7bf90ff4
Venugopal Reddy <venugopalreddy1895@gmail.com>
2025-11-01 23:04:58 +0530

    building the Frontend Ui

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,9 +2,17 @@ import "./App.css";
 import Sidebar from "./Sidebar";
 import ChatWindow from "./ChatWindow";
 import { MyContext } from "./Mycontex";
+import { useState } from "react";
+
+
 
 function App() {
-  const providerValues ={};
+  const [prompt, setPrompt] = useState("");
+  cosnt [reply , setReply] = useState(null);
+  const providerValues ={
+    prompt,setPrompt,
+    reply,setReply
+  };
     return ( 
         <div className="app">
           <MyContext.Provider value={providerValues}>
